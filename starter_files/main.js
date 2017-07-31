cf406573a39b4fac357201262f91f04e20d6b914
Courtney <courtney.trochim@gmail.com>
2017-07-31 18:04:43 -0500

    added more else statements

--- starter_files/main.js
+++ starter_files/main.js
@@ -99,20 +99,12 @@ let formData = [
 let formHTML = document.getElementById('fields');
 for (let i = 0; i < formData.length; i++) {
 
-  let inputFOO = document.createElement('input');
-  formHTML.appendChild(inputFOO);
-  inputFOO.setAttribute('type', formData[i].type);
-  inputFOO.setAttribute('placeholder', formData[i].label);
-  inputFOO.setAttribute('id', formData[i].id);
-  inputFOO.setAttribute('icon', formData[i].icon);
-  inputFOO.setAttribute('options', formData[i].options);
-
   if (formData.type === 'select') {
     let selectLang = document.createElement('select');
     let selectlist = document.createElement('option');
     let selLangOpts = formData[i].options;
 
-    selectLang.appendChild(selectlist)
+    selectLang.appendChild(selectlist);
 
     for (let j = 0; j < dataForm.length; j++) {
       let newOptions = document.createElement('option')
@@ -123,24 +115,29 @@ for (let i = 0; i < formData.length; i++) {
     formHTML.appendChild(selectLang)
 
   }
+  if
+  else formData.type === textarea {
+    let textBox = document.createElement('textarea');
+    textBox.setAttribute('cols', 'rows');
+  }
 
-} else formData.type === textarea {
-  setAttribute(textarea)
+  formHTML.appendChild(textBox)
+
+  else {
+    let inputFOO = document.createElement('input');
+    formHTML.appendChild(inputFOO);
+    inputFOO.setAttribute('type', formData[i].type);
+    inputFOO.setAttribute('placeholder', formData[i].label);
+    inputFOO.setAttribute('id', formData[i].id);
+    inputFOO.setAttribute('icon', formData[i].icon);
+    inputFOO.setAttribute('options', formData[i].options);
+  }
+
 
 }
 
-}
 
-let langOptions = document.getElementById(formData[4].options)
-}
-// let langOptions = [];
-// for (let j = 0; j < formData.length; j++) {
-//   landOptions.push(formData[4].options.label, formData[4].options.value)
-// }
 
-// for (let j = 0; j < formData.length; j++) {
-//   let labelNames = formData.label. {
-//     labelNames.push(formData.label[j].)
 //
 //find every instance of label
 //make every innstance of label = that iteration of //label's value
